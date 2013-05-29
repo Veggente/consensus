a04d94e32eb487d5cc88c24e0d38dce190d579f0
Veggente <veggente@gmail.com>
2013-05-29 16:03:28 -0700

    first try

---
+++ consensus.m
@@ -0,0 +1,7 @@
+% Consensus algorithm for getting a better lower bound of the real-time
+% local-pooling factor
+clear all; clc;
+
+% Generate a random network
+% Initialization of neighborhood information
+% Convergence to consensus
